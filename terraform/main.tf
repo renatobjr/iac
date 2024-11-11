98a8f7c7063e27e7fd2cade4900ff75fb25cef06
renatobjr <renato.bonfim.jr@cciao.org>
2024-11-11 10:12:47 -0300

    bugfix: resource name aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "" {
+resource "aws_instance" "windows-server-2019" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
